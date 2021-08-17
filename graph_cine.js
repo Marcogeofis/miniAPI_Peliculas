986b013d785ccdd1378fdeb2f5e9d5b198a8afd8
Marco Rivera <marcogeofis@gmail.com>
2021-08-17 15:28:11 -0500

    se prepararon las graficas de cines y streaming, también se agrego el archivo styles.css y procesos_datos.js

---
+++ graph_cine.js
@@ -0,0 +1,12 @@
+export default{
+    "Cinemas": [
+        {
+            "Cine": "Cinemex",
+            "costo": 48
+        },
+        {
+            "Cine": "Cinepolis",
+            "costo": 75
+        },
+    ]
+}
---
+++ graph_streming.js
@@ -0,0 +1,28 @@
+export default{
+    "streaming": [
+        {
+            "plataforma": "Netflix",
+            "costo": 196,
+        },
+        {
+            "plataforma": "Amazon",
+            "costo": 99,
+        },
+        {
+            "plataforma": "HBOmax",
+            "costo": 99,
+        },
+        {
+            "plataforma": "Disney+",
+            "costo": 159,
+        },
+        {
+            "plataforma": "Blim",
+            "costo": 109,
+        },
+        {
+            "plataforma": "YouTube Prime",
+            "costo": 119,
+        },
+    ]
+}
